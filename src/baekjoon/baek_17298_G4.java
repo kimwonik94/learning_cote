360bbe76531ce42a68716657ebb570b936c81ae3
kwi_pg <whitedogkim@gmail.com>
2024-06-07 16:44:40 +0900

    17298 시간초과 이유 파악

--- src/baekjoon/baek_17298_G4.java
+++ src/baekjoon/baek_17298_G4.java
@@ -1,13 +1,46 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class baek_17298_G4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        int[] a = new int[n];
+        int[] answer = new int[n];
+        StringBuffer stringBuffer = new StringBuffer();
+        String[] str = bufferedReader.readLine().split(" ");
+        for(int i =0; i<a.length; i++){
+            a[i] = Integer.parseInt(str[i]);
+        }
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(0);
+        for(int i =1; i<n; i++){
+            while (!stack.empty() && a[stack.peek()] < a[i]){
+                answer[stack.pop()] = a[i];
+            }
+            stack.push(i);
+        }
+
+        while(!stack.empty()){
+            answer[stack.pop()] = -1;
+        }
+
+        for(int i =0; i < answer.length; i++){
+            stringBuffer.append(answer[i] + " ");
+        }
+
+        System.out.println(stringBuffer.toString());
+
+
         // 제한 시간 1초 처음에는 스택 없이 풀수 있는 간단 한 문제라고 생각하며 품
         // 당연히 시간 초과가 나옴 최악의 경우는 n2이라...
-        Scanner sc = new Scanner(System.in);
+       /* Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
         int[] answerArr = new int[n];
@@ -47,7 +80,7 @@ public class baek_17298_G4 {
         System.out.println(stringBuffer.toString());
 
 
-        /*for(int i = 0; i< arr.length; i++){
+        for(int i = 0; i< arr.length; i++){
             int index = i+1;
             int result = -1;
             while(index < arr.length){
